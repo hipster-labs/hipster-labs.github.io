9f08166c7981197fc88e0038c23488070316b378
deepu105 <d4udts@gmail.com>
2020-09-09 23:18:02 +0200

    built JDL studio 9-sep-2020

---
+++ precache-manifest.c0deadab3038bf0b0cc3fe811eb6e218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fe0588ad5acdd7b3e0e11d6be2adb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b88fc006bf3a7623c6f7",
+    "url": "/static/css/2.dd21d29c.chunk.css"
+  },
+  {
+    "revision": "7069bbabbbdb8d3d7c31",
+    "url": "/static/css/main.69245b63.chunk.css"
+  },
+  {
+    "revision": "b88fc006bf3a7623c6f7",
+    "url": "/static/js/2.51d841b7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.51d841b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7069bbabbbdb8d3d7c31",
+    "url": "/static/js/main.7cb08961.chunk.js"
+  },
+  {
+    "revision": "571532b9a41d1139faa2",
+    "url": "/static/js/runtime-main.3037351d.js"
+  }
+]);
